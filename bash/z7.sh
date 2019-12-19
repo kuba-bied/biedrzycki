6a0596aa2afc5b976a6a5159c96ae3cae1d8673c
biedrzycki <pol-2004@o2.pl>
2019-12-19 09:23:24 +0100

    function,expression

--- bash/z7.sh
+++ bash/z7.sh
@@ -15,3 +15,17 @@ while [ $x -le 10 ]; do
      Moja
 x=$[x + 1]
 done
+
+#wyrażenia arytmertyczne
+echo $[2+2]
+echo $((2+2))
+x=5
+y=2
+let suma=x+y
+echo ${suma}
+#operatory arytmetyczne
+# + : dodawanie
+# - : odejmowanie
+# * : mnożenie
+# / : dzielenie
+# % : reszta z dzielenia
