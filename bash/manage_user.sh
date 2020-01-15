0f594f9c6e7ac8db805ca37eecd154a62bfdddae
biedrzycki <pol-2004@o2.pl>
2020-01-15 12:34:41 +0100

    srypt end

--- bash/manage_user.sh
+++ bash/manage_user.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 user_list=(`cat users.txt`)
@@ -8,49 +7,74 @@ function showUsers() {
     #Ładowanie danych z pliku
     user_list=(`cat users.txt`)
     for (( i=0; i<=${#user_list[@]}; i++ ))
-do
+    do
 	echo "	${user_list[i]}"
     done
 }
 
+
 function addUsers() {
     echo "addUsers..."
-for user in "${user_list[@]}"
-do
-	echo "Add user: ${user}"
-	useradd ${user} -s /sbin/nologin -g "users"
-done
+    echo "Are you sure? [y/n]"
+    read sure 
+    if [ "${sure}" == "y" ]; then
+    for user in "${user_list[@]}" 
+    do
+	echo "Add user ${user} [ok]"
+	sudo useradd ${user} -s /sbin/nologin -g "users"
+    done
+    fi
 }
+
+
 function delUsers() {
-    echo "delUsers..."
+    echo "delUsers"
+    echo "Are you sure? [y/n]"
+    read sure 
+    if [ "${sure}" == "y" ]; then
+    for user in "${user_list[@]}" 
+    do
+	echo "Remove user ${user} [ok]"
+	sudo userdel ${user}
+    done
+    fi
 }
+
+
 function acceptRemoteLogin() {
     echo "acceptRemoteLogin..."
+for user in "${user_list[@]}" 
+    do
+	echo "Accept remote login for ${user} [ok]"
+	sudo  usermod -s  /sbin/bash ${user}
+    done
 }
 function denieRemoteLogin() {
     echo "denieRemoteLogin..."
-}
-function help() 
-{
-    cat << EndOfMessage
-    Opis opcji skryptu:
-    ------------------
-SU -> listowanie urzytkowników
-AU -> dodawanie urzytkownika
-DU -> usuwanie urzytkownika
-EndOfMessage
+for user in "${user_list[@]}" 
+    do
+	echo "Denied remote login for ${user} [ok]"
+	sudo  usermod -s  /sbin/nologin ${user}
+    done
 }
 
+function help() {
+cat << EndOfMessage
+    Opis opcji skryptu:
+    ------------------
+SU -> listowanie urzytkowników|1
+AU -> dodawanie urzytkownika  |2
+DU -> usuwanie urzytkownika   |3
+EndOfMessage
+}
 
 function Quit() {
     exit 0
 }
 
 #menu
-
 select option in  SU AU DU ARL DRL HELP QUIT
 do 
-
     case ${option} in
 	"SU") showUsers ;;
 	"AU") addUsers ;;
